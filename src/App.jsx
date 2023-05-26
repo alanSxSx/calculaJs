53891c07a33ffc34f180327983819958d17fd554
Alan Barbosa <alanzinhob@hotmail.com>
2023-05-26 15:44:43 -0300

    otimizado

--- src/App.jsx
+++ src/App.jsx
@@ -43,20 +43,7 @@ const HandleClear = () => {
     <>
     <h3>Calculadora</h3>
      <Visor nome="Visor1" tipo="Text" valor={visor} /><br/><br/>
-     <Botao texto="7" valor={7} eclick={HandleButon}/>
-     <Botao texto="8" valor={8} eclick={HandleButon}/>
-     <Botao texto="9" valor={9} eclick={HandleButon}/>
-     <Botao texto="+" valor={'+'} eclick={HandleButon}/><br/>
-     <Botao texto="4" valor={4} eclick={HandleButon}/>
-     <Botao texto="5" valor={5} eclick={HandleButon}/>
-     <Botao texto="6" valor={6} eclick={HandleButon}/>
-     <Botao texto="-" valor={'-'} eclick={HandleButon}/><br/>
-     <Botao texto="1" valor={1} eclick={HandleButon}/>
-     <Botao texto="2" valor={2} eclick={HandleButon}/>
-     <Botao texto="3" valor={3} eclick={HandleButon}/>
-     <Botao texto="*" valor={'*'} eclick={HandleButon}/><br/>
-     <Botao texto="0" valor={'0'} eclick={HandleButon}/>
-     <Botao texto="/" valor={'/'} eclick={HandleButon}/>
+     <BotoesCalc Handlebuttons={HandleButon}/>
      <Botao texto="=" valor={'='} eclick={HandleResult}/>
      <Botao texto="C" valor={'C'} eclick={HandleClear}/>
     </>
--- src/Components/BotoesCalc.jsx
+++ src/Components/BotoesCalc.jsx
@@ -1,26 +1,25 @@
 import Botao from "./Botao";
 
-function BotoesCalc() {
+function BotoesCalc({Handlebuttons}) {
 
 
   return (
     <>
-    <Botao texto="7" valor={7} eclick={HandleButon}/>
-     <Botao texto="8" valor={8} eclick={HandleButon}/>
-     <Botao texto="9" valor={9} eclick={HandleButon}/>
-     <Botao texto="+" valor={'+'} eclick={HandleButon}/><br/>
-     <Botao texto="4" valor={4} eclick={HandleButon}/>
-     <Botao texto="5" valor={5} eclick={HandleButon}/>
-     <Botao texto="6" valor={6} eclick={HandleButon}/>
-     <Botao texto="-" valor={'-'} eclick={HandleButon}/><br/>
-     <Botao texto="1" valor={1} eclick={HandleButon}/>
-     <Botao texto="2" valor={2} eclick={HandleButon}/>
-     <Botao texto="3" valor={3} eclick={HandleButon}/>
-     <Botao texto="*" valor={'*'} eclick={HandleButon}/><br/>
-     <Botao texto="0" valor={'0'} eclick={HandleButon}/>
-     <Botao texto="/" valor={'/'} eclick={HandleButon}/>
-     <Botao texto="=" valor={'='} eclick={HandleResult}/>
-     <Botao texto="C" valor={'C'} eclick={HandleClear}/>
+    <Botao texto="7" valor={7} eclick={Handlebuttons}/>
+     <Botao texto="8" valor={8} eclick={Handlebuttons}/>
+     <Botao texto="9" valor={9} eclick={Handlebuttons}/>
+     <Botao texto="+" valor={'+'} eclick={Handlebuttons}/><br/>
+     <Botao texto="4" valor={4} eclick={Handlebuttons}/>
+     <Botao texto="5" valor={5} eclick={Handlebuttons}/>
+     <Botao texto="6" valor={6} eclick={Handlebuttons}/>
+     <Botao texto="-" valor={'-'} eclick={Handlebuttons}/><br/>
+     <Botao texto="1" valor={1} eclick={Handlebuttons}/>
+     <Botao texto="2" valor={2} eclick={Handlebuttons}/>
+     <Botao texto="3" valor={3} eclick={Handlebuttons}/>
+     <Botao texto="*" valor={'*'} eclick={Handlebuttons}/><br/>
+     <Botao texto="0" valor={'0'} eclick={Handlebuttons}/>
+     <Botao texto="/" valor={'/'} eclick={Handlebuttons}/>
+    
     </>
   )
 }
